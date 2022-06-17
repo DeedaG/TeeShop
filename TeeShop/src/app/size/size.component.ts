5505e762f2754ab1d2ad31dcc732577b21d60680
christiatidwell <ChristiaTidwell@gmail.com>
2022-06-17 17:22:15 -0500

    color folder/ beginning structure

---
+++ TeeShop/src/app/size/size.component.ts
@@ -0,0 +1,9 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-size',
+  templateUrl: './size.component.html',
+})
+export class SizeComponent {
+}
